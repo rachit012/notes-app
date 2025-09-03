56b35df7699445b1656f6ff413d7811405f90640
rachit012 <rachitthakur012@gmail.com>
2025-09-03 21:05:07 +0530

    fixed server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -13,10 +13,20 @@ configurePassport();
 const app = express();
 const PORT = process.env.PORT || 5001;
 
+const allowedOrigins = [process.env.FRONTEND_URL];
+
 const corsOptions = {
-  origin: process.env.FRONTEND_URL, 
-  optionsSuccessStatus: 200
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('This origin is not allowed by CORS'));
+    }
+  },
+  optionsSuccessStatus: 200,
 };
+
+app.options('*', cors(corsOptions));
 
 app.use(cors(corsOptions));
 
